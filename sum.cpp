1e29d4308092ec9a2a167dbf9bc67d204927bd7d
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-06 11:33:19 +0530

    modified sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int add_numbers(int a, int b){
+    int sum = a + b;
+    return sum;
+}
+
 int main(){
     int number1 = 10;
-    int number2 = 20;
+    int number2 {20};
     int sum = number1 + number2;
     cout << sum << endl;
+    cout << add_numbers(2,5) << endl;
+
     return 0;
 }
+
