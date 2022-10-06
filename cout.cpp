946b112284c5e9d1ff38393204957176fcc75e7b
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-06 10:11:34 +0530

    added sum.cpp

--- cout.cpp
+++ cout.cpp
@@ -5,7 +5,7 @@ int main(){
     std::cout << "Number 2" << std::endl;
 
     for (int i = 3;i<=10;i++){
-        std::cout << "Hello" << std::endl;
+        std::cout << "Number " << i << std::endl;
     }
 
     return 0;
---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number1 = 10;
+    int number2 = 20;
+    int sum = number1 + number2;
+    cout << sum << endl;
+    return 0;
+}
