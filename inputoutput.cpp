3afdea7bd3fb06dae35b88924fe962c21eabe8b3
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-06 12:00:06 +0530

    added inputoutput.cpp

---
+++ inputoutput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout << "Enter a number : ";
+    cin >> n1;
+    cout << "Enter another number : ";
+    cin >> n2;
+    cout << (n1 + n2) << endl;
+}
--- sum.cpp
+++ sum.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+//using namespace std;
 
 int add_numbers(int a, int b){
     int sum = a + b;
@@ -10,8 +10,10 @@ int main(){
     int number1 = 10;
     int number2 {20};
     int sum = number1 + number2;
-    cout << sum << endl;
-    cout << add_numbers(2,5) << endl;
+    std::cout << sum << std::endl;
+    std::cout << add_numbers(2,5) << std::endl;
+    std::cout << add_numbers(21,54) << std::endl;
+    std::cout << add_numbers(62,45) << std::endl;
 
     return 0;
 }
