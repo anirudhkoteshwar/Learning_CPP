e517abc6c0f8f7a2b68bbf8457c2db4816ed9d36
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-12 21:16:00 +0530

    modified varsize

--- varsize.cpp
+++ varsize.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main(){
-    cout << "Size of int  : " << sizeof(int) << endl;
+    cout << "Size of int : " << sizeof(int) << endl;
+    cout << "Size of float : " << sizeof(float) << endl;
+    cout << "Size of long : " << sizeof(long) << endl;
+    cout << "Size of double : " << sizeof(double) << endl;
+    cout << "Size of short : " << sizeof(short) << endl;
 }
