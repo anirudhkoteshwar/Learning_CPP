1e11efac40609eb665bccc30c7e9dddcf7968082
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-12 20:59:26 +0530

    modified inputoutput.cpp

--- inputoutput.cpp
+++ inputoutput.cpp
@@ -11,7 +11,7 @@ int main(){
     std::cin >> n2;
     std::cout << "The sum is " << (n1 + n2) << std::endl;
     std::cout << "Whats your name and age ? " << std::endl;
-    std::getline(std::cin >> std::ws,name); //the std::ws is needed to extract the whitespaces too
+    std::getline(std::cin >> std::ws, name); //the std::ws is needed to extract the whitespaces too
     //std::cin >> std::ws >> name; //this doesn't work 
     std::cin >> age;
     std::cout << "Hello, " << name << "! You're " << age << " years old." << std::endl;
