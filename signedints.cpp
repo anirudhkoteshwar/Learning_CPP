6a748682f33a5ea66652a6e7b3b8114dcdc2efbb
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-13 11:01:28 +0530

    modified signedints.cpp

--- signedints.cpp
+++ signedints.cpp
@@ -5,6 +5,10 @@ int main(){
     int n = 10;
     int x = -20; //ints are signed by default
     signed int y = -264;
+    signed z = -2048;
+    signed long a = -124124341123;
     cout << n << " " << x << " " << y << endl;
     cout << sizeof(n) << " " << sizeof(x) << " " << sizeof(y) << endl;
+    cout << z << " " << sizeof(z) << endl;
+    cout << a << " " << sizeof(z) << endl;
 }
