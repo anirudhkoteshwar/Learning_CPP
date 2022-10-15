9806d41f064cb67c7ec08250a5c5a735aea295aa
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-15 18:01:33 +0530

    added auto.cpp

---
+++ auto.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main(){
+    auto n1 {12};
+    auto n2 {12.43};
+    auto n3 {'e'};
+    auto n4 {true};
+
+    cout << n1 << endl;
+    cout << n2 << endl;
+    cout << n3 << endl;
+    cout << n4 << endl;
+ }
