3709ec2607c7e03001799b2aa044c48bb3cf2945
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-15 18:23:50 +0530

    added operations.cpp

---
+++ operations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1 = 10;
+    int n2 = 20;
+    cout << n1 + n2 << endl;
+    cout << n2 - n1 << endl;
+    cout << n1 * n2 << endl;
+    cout << n2 / (n1+1) << endl;
+    cout << n2 % n1 << endl;
+}
