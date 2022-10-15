426ee6554eb197833f042679a6cd3b3a4350351f
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-15 17:56:23 +0530

    modified characters.cpp

--- characters.cpp
+++ characters.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
-    char A = 66;
+    char A = 66; //uses ascii
     char B = 65;
     cout << A << endl;
     cout << B << endl;
+    cout << static_cast<int>(A) << endl;
+    cout << static_cast<int>(B) << endl;
 }
