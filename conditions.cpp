c2fd22451400dc594f784c7854a96b0e9bc597da
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-15 17:45:45 +0530

    added conditionals.cpp

---
+++ conditions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool state;
+    cin >> state;
+    if (state){
+        cout << "State is true" << endl;
+    }
+    else { 
+        cout << "State is false" << endl;
+    }
+
+}
