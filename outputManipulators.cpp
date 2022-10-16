80d2d37c0853c1ebba651e5765a95bb09fa9025d
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-16 14:30:09 +0530

    modified outputManipulators.cpp

--- outputManipulators.cpp
+++ outputManipulators.cpp
@@ -10,10 +10,24 @@ int main(){
     cout << "Hel" << flush << "lo " << "world" << endl;
 
     //std::setw sets width of output
+    //std::right sets the justification
+    //std::internal puts - with left justification and the number with right for -3134 -->  -  3134
+    cout << right;
     cout << setw(10) << "Firstname"  << setw(10) << "Lastname" << setw(7) << "Grades" << endl;
     cout << setw(10) << "John"  << setw(10) << "Doe" << setw(5) << "88" << endl;
     cout << setw(10) << "Augustus"  << setw(10) << "Gloop" << setw(5) << "73" << endl;
     cout << setw(10) << "Kiki"  << setw(10) << "Kaka" << setw(5) << "91" << endl;
     cout << setw(10) << "Capy"  << setw(10) << "Bara" << setw(5) << "93" << endl;
+    cout << endl;
+    cout << left;
+    //std::setfill() used to replace spaces with some fill character
+    cout << setfill('-');
+    cout << setw(10) << "Firstname"  << setw(10) << "Lastname" << setw(7) << "Grades" << endl;
+    cout << setw(10) << "John"  << setw(10) << "Doe" << setw(5) << "88" << endl;
+    cout << setw(10) << "Augustus"  << setw(10) << "Gloop" << setw(5) << "73" << endl;
+    cout << setw(10) << "Kiki"  << setw(10) << "Kaka" << setw(5) << "91" << endl;
+    cout << setw(10) << "Capy"  << setw(10) << "Bara" << setw(5) << "93" << endl;
+
+
     
 }
