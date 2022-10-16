d8ba43d2c684e3ed690ce02dee907c51c5770951
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-16 13:44:12 +0530

    added compoundOperators.cpp

---
+++ compoundOperators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    x+=5;
+    cout << "value of x+=5 is : " << x << endl;
+    x=10;
+    x-=5;
+    cout << "value of x-=5 is : " << x << endl;
+    x=10;
+    x*=5;
+    cout << "value of x*=5 is : " << x << endl;
+    x=10;
+    x/=5;
+    cout << "value of x/=5 is : " << x << endl;
+    x=10;
+    x%=5;
+    cout << "value of x%=5 is : " << x << endl;
+
+}
