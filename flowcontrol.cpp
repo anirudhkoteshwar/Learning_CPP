8785bfc5be06c4c9491258bce9f1246dac22c18d
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-19 07:12:18 +0530

    modified flowcontrol.cpp

--- flowcontrol.cpp
+++ flowcontrol.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+     int a {10};
+     int b {20};
+     if (a==b){
+        cout << "True" << endl;
+     }
+     else if (!(a==b)){
+        cout  << "False" << endl;
+     }
+}
