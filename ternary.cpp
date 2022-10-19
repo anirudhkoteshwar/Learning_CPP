6b23459cc39be1701ecf355f085950956d0f0b40
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-19 07:37:20 +0530

    added ternary.cpp

---
+++ ternary.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    //result = (condition) ? option1 : option2 ;
+    
+    //equivalent in if and else is
+    /*
+    if(condition){
+        result = option1;
+    }
+    else{
+        result = option2;
+    }
+    */
+   
+}
