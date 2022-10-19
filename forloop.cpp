e6fa3ecc5797e77ded8f505390d5c2a982e3c3bb
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-19 10:54:30 +0530

    modified forloop.cpp

--- forloop.cpp
+++ forloop.cpp
@@ -13,6 +13,7 @@ int main(){
    for (size_t i{}; i < 11;++i){
     cout << "Hello " << i << endl;
    }
+   cout << "Size of size_t is : "<<sizeof(size_t) << " Bytes" << endl;
     //we can use size_t as an alias for unsigned int, and it is used commonly for iterators
     //size_t i; and size_t i{}; are not the same. the second one sets i to 0
 }
