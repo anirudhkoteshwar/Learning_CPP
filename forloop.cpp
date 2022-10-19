488b289b451227bf5988f95d90547bc0efb72ed2
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-19 10:48:26 +0530

    added forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+    for loops have these components
+        -iterator
+        -starting point
+        -test
+        -increment/decrement
+        -body
+    */
+   for (int i=0; i < 11;++i){
+    cout << "Hello " << i << endl;
+   }
+}
