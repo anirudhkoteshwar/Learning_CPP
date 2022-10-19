44def8c6f5446cd3bca3153da8bf34281c2d8270
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-10-19 07:40:26 +0530

    added ternary.cpp

--- ternary.cpp
+++ ternary.cpp
@@ -14,5 +14,10 @@ int main(){
         result = option2;
     }
     */
-   
+   //option 1 and 2 must be of same type or the types must be convertible
+
+    int a = 5;
+    int b = 6;
+    int c = (a==b) ? 10 : 20;
+    cout << c << endl;
 }
