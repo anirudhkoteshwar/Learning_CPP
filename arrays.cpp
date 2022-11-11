d7f56b9e80471497cacbdc6639fec03cde3032d1
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-11 21:45:09 +0530

    modified array.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 
 int main(){
-    
+    // arrays will have junk values after they are initialized.
+    // C++ will allow moving out of the arrays bounds but this will break the program. So this must be avoided. 
 }
