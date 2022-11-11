c495f8031b7aded64be2c83e90776dcdbdaf829d
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-11 22:04:14 +0530

    modified array.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -15,9 +15,15 @@ int main(){
     }
 
     //arrays can be initialize and values can be set on the same line
+    // arrays can be initialized without being completely full. The other values are initialized to 0.
     double salary[10] {1.1,12.3,123.4,5.43,4.53,5454.235,53523.254,43.553,5353.53,4.53};
+    double money[5] {1,23,4};
 
     for (int i = 0;i < 10; ++ i) { //read data
-        cout << "salary[" << i << "]" << "=" << salary[i] << endl;
+        cout << "salary[" << i << "]" << "=" << salary[i] << endl; 
+    }
+
+    for (int i = 0;i < 5; ++ i) { //read data
+        cout << "money[" << i << "]" << "=" << money[i] << endl; 
     }
 }
