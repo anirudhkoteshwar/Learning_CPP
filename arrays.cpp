9f03583fe60b2729803af7e882ba3e369bcbb18a
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-11 21:58:13 +0530

    modified array.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -13,4 +13,11 @@ int main(){
     for (int i = 0;i < 10; ++ i) { //read data
         cout << "score[" << i << "]" << "=" << scores[i] << endl;
     }
+
+    //arrays can be initialize and values can be set on the same line
+    double salary[10] {1.1,12.3,123.4,5.43,4.53,5454.235,53523.254,43.553,5353.53,4.53};
+
+    for (int i = 0;i < 10; ++ i) { //read data
+        cout << "salary[" << i << "]" << "=" << salary[i] << endl;
+    }
 }
