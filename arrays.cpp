14a37eff2461280d919ed955187fe0485a7bc0a3
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-11 21:53:00 +0530

    modified array.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main(){
     // arrays will have junk values after they are initialized.
-    // C++ will allow moving out of the arrays bounds but this will break the program. So this must be avoided. 
+    // C++ will allow moving out of the arrays bounds but this will break the program. So this must be avoided.
+
+    int scores[10];
+
+    for (int i = 0;i < 10; ++i) { //write data
+        scores[i] = i*3;
+    }
+    for (int i = 0;i < 10; ++ i) { //read data
+        cout << "score[" << i << "]" << "=" << scores[i] << endl;
+    }
 }
