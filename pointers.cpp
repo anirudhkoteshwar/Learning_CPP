5a76878af37bc4833f75ccab35bf20533a7ebd18
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-17 11:20:16 +0530

    modified array.cpp

---
+++ pointers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //pointer variables store memory addresses of the specified type
+    int *p; //pointer which stores address of integer.
+    int a = 10;
+    p = &a; //store address of integer a in pointer p
+    cout << p <<endl; //address of p
+    cout << *p << endl; //value stored at p
+}
