8e6cbd4d5ab74a5dba8bfe01f14f58ad9ad4bc0c
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-17 11:22:32 +0530

    modified pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
     //pointer variables store memory addresses of the specified type
-    int *p; //pointer which stores address of integer.
-    int a = 10;
+    int *p {}; //pointer which stores address of integer. CAN ONLY STORE ADDRESS OF INTEGER.
+    int a {10};
     p = &a; //store address of integer a in pointer p
     cout << p <<endl; //address of p
     cout << *p << endl; //value stored at address p
