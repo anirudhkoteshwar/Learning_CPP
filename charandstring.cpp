08c3e900738de0d5c620942899d1543183421dd0
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-27 20:13:01 +0530

    modified charandstring.cpp

--- charandstring.cpp
+++ charandstring.cpp
@@ -3,8 +3,18 @@ using namespace std;
 
 int main(){
     //chech if a character is alphanumeric
+
     std::cout << "Is 'c' alphanumeric ? : " << std::isalnum('c') << std::endl; //nonzero value means true
     std::cout << "Is '^' alphanumeric ? : " << std::isalnum('^') << std::endl; //zero means false
+
+
+    char test{'*'};
+    if (isalnum(test)){
+        cout << test << " is an alphanumeric character" << endl;
+    }
+    else {
+        cout << test << " is not an alphanumeric character" << endl;
+    }
 
     return 0;
 }
