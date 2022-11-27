8d5f2402ea258efec06e5c21905523309b86b791
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-27 13:46:38 +0530

    modified dynamicarrays.cpp

---
+++ dynamicarrays.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //allocate arrays on the heap
+    int size {10};
+    double* p_salaries {new double[size]}; //array will contain garbage values
+    int* p_students {new(nothrow) int[size]{}}; //all values initialized to 0
+    double* p_scores {new(nothrow) double[size]{10,20,30,40,50}}; //first 5 values are set and the rest are 0
+    
+    //to access the values
+    if(p_scores){ //if the pointer is not a null pointer
+        for (int i{};i<size;++i){
+            cout << "Value : " << p_scores[i] << " : " << *(p_scores + i) << endl; //first one is array form. second one is pointer form
+        }
+    }
+
+    //to release the memory,
+    delete[] p_salaries;
+    p_salaries = nullptr;
+
+    delete[] p_students;
+    p_students = nullptr;
+
+    delete[] p_scores;
+    p_scores = nullptr;
+    
+    return 0;
+}
