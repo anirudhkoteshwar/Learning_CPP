9f8ac47ae292ada7f4a08d6740a385461250d4f0
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-27 20:22:19 +0530

    modified charandstring.cpp

--- charandstring.cpp
+++ charandstring.cpp
@@ -16,5 +16,32 @@ int main(){
         cout << test << " is not an alphanumeric character" << endl;
     }
 
+    //check if a character is an alphabet
+    char test2 {'a'};
+    if (isalpha(test2)){
+        cout << test2 << " is an alphabet character" << endl;
+    }
+    else {
+        cout << test2 << " is not an alphabet character" << endl;
+    }
+
+    if (isalpha(test)){
+        cout << test << " is an alphabet character" << endl;
+    }
+    else {
+        cout << test << " is not an alphabet character" << endl;
+    }
+
+    //check if a character is a blank
+    char message[] {"Hello this is a test message."};
+    int blank_count {0};
+    for (size_t i{0}; i < size(message); ++i){
+        if (isblank(message[i])) {
+            cout << "Found a blank at index " << i << endl;
+            ++blank_count;
+        }
+    }
+    cout << "Found " << blank_count << " blanks in total." << endl;
+
     return 0;
 }
