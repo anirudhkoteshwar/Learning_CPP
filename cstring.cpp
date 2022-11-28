0e059b8497fa0a54d7b2624b9c414b19938a0975
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-28 11:13:26 +0530

    modified cstring.cpp

--- cstring.cpp
+++ cstring.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main(){
     const char msg1[] {"The sky is blue."};
-    const char* msg2 {"The sky is red."};
+    const char* msg2 {"The sky is blue."};
     cout << "message 1 : " << msg1 << endl;
     cout << "message 2 : " << msg2 << endl;
+    cout << "strlen(msg1) : " << strlen(msg1) << endl; //ignores null character at the end
+    cout << "sizeof(msg1) : " << sizeof(msg1) << endl; //includes null character at the end
 }
