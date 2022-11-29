ee175c18262473f3036d6c1fa63353d3a9b8976b
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-29 06:52:44 +0530

    modified stdstring.cpp

---
+++ stdstring.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    //character arrays are annoying as you need to keep their size in mind
+    //cstrings but much better
+    string name {"Anirudh Koteshwar"};
+    cout << name << endl;
+}
