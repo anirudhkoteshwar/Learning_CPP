44b0e93f6e4c5bfab7139734a931dfd917004fe5
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-11-30 10:23:35 +0530

    modified stdstring.cpp

--- stdstring.cpp
+++ stdstring.cpp
@@ -6,6 +6,7 @@ int main(){
     //character arrays are annoying as you need to keep their size in mind
     //cstrings but much better
     string name {"Anirudh Koteshwar"};
+    string name1 {name}; //initialize with exisiting strnig
     string msg(5,'e'); // initialize with multiple copies. Us () instead of {}
     string greeting {"Hello world",3,4}; //starts at index 3 and takes 4 characters
     cout << name << endl << msg << endl << greeting << endl;
