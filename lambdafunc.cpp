a276ddf586087008deb209ec20c2c8510a848fe1
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-02 21:43:30 +0530

    modified lambdafunc.cpp

--- lambdafunc.cpp
+++ lambdafunc.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main(){
     //lambda functions are anonymous functions (w/o names)
-    
+    //lambda function signature:
+    /*
+        [capture list] (parameters) -> return type {
+            body
+        }
+    */
+   auto func = [](){
+    cout << "Hello world" << endl;
+   };
+   func();
 }
