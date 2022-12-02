991d2b16847a55b3386e8cd89b7123b4ecc65d11
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-02 21:47:21 +0530

    modified lambdafunc.cpp

--- lambdafunc.cpp
+++ lambdafunc.cpp
@@ -13,4 +13,14 @@ int main(){
     cout << "Hello world" << endl;
    };
    func();
+
+   //you can also call a lambda function without assigning it to a variable by using () after closing the body
+   [](){
+    cout << "This is a lambda function " << endl;
+   }();
+
+   //you put parameters in the first parentheses
+   [](double a, double b){
+    cout << "a+b = " << a+b << endl;
+   }(12.1,34.5);
 }
