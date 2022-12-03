6fb43926f32704b4d34edb8bea32ca68881945d2
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-03 20:33:42 +0530

    modified functiontemplates.cpp

--- functiontemplates.cpp
+++ functiontemplates.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+template <typename T> T maximum(T a, T b);
+
 int main(){
-    
+    //useful for setting up the same function for different datatypes.
+    //you need to prototype templates    
+    int a {10};
+    int b {20};
+    double c {22.3};
+    double d {302.34};
+    cout << maximum(a,b) << endl;
+    cout << maximum(c,d) << endl;
+}
+
+template <typename T> T maximum(T a, T b){
+    return (a>b) ? a:b;
 }
