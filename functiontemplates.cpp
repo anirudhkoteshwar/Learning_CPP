5c59d805577159ed1226e10213562407fd907ae9
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-04 18:12:54 +0530

    modified functiontemplates.cpp

--- functiontemplates.cpp
+++ functiontemplates.cpp
@@ -5,6 +5,9 @@ using namespace std;
 template <typename T> T maximum(T a, T b);
 template <typename T> const T& minimum( const T& a, const T& b);
 
+//template specialization: set specific rules for certain datatypes
+template <> const char* maximum<const char*> (const char*,const char*);
+
 int main(){
     //useful for setting up the same function for different datatypes.
     //you need to prototype templates    
@@ -13,10 +16,12 @@ int main(){
     double c {22.3};
     double d {302.34};
     //we are using strings but we need to make sure the operations performed in the template are valid for strings
-    string e {"Hello"};
-    string f {"World"};
+    const char* e {"Hello"};
+    const char* f {"World"};
     cout << maximum(a,b) << endl;
     cout << maximum(c,d) << endl;
+
+    //use the specalized comparision
     cout << maximum(e,f) << endl;
 
     //you can explicitly say which type you want the template to use. if you enter convertible types, you also get convertible types
@@ -28,7 +33,12 @@ template <typename T> T maximum(T a, T b){
     return (a>b) ? a:b;
 }
 
-// work by reference (use the external variable not a copy)
+// work by reference (use the external variable not a copy but we cant modify it as we are using const)
 template <typename T> const T& minimum( const T& a, const T& b){
     return (a>b) ? a:b;
+}
+
+//body of template specialization
+template <> const char* maximum<const char*> (const char* a,const char* b){
+    return (strcmp(a,b) > 0) ? a : b ;
 }
