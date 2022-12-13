63b600b7b59f2401d2b867dd0166b9465c320f57
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-13 20:29:57 +0530

    modified map.cpp

--- map.cpp
+++ map.cpp
@@ -17,7 +17,22 @@ int main() {
     //inserting items to maps
     m1['d'] = 4;
     m1.insert(pair<char,int>('e',5)); //pairs just combine two values which can be of different datatypes
-    
 
+    //erase elements
+    m1.erase('d');
+    
+    //clear a map
+    //m1.clear()
+
+    //check if map is empty
+    cout << m1.empty() << endl; //1 if empty 0 if not
+
+    //get size of a map
+    cout << m1.size() << endl;
+
+    //iterating through a map
+    for (map<char,int>::iterator i = m1.begin(); i != m1.end(); ++i){ //m1.begin() points the iterator to the first item and ++i moves the iterator to the next item. you could use auto instead just for shorter code.
+    cout << (*i).first << " " << (*i).second << '\n'; //dereference the iterator and get the first or second values. 
+    }
 
 }
