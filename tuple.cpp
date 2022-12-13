fa03753641a6cd02174a1f3245ae281f815eead0
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-13 18:05:37 +0530

    modified tuple.cpp

--- tuple.cpp
+++ tuple.cpp
@@ -7,4 +7,24 @@ int main(){
     cout << get<1>(person) << endl; //access elements through get<index>(tuple name)
     get<1>(person) = "Banana"; //set elements through get<index>(tuple name)
     cout << get<1>(person) << endl;
+
+    tuple<int,char,float,bool> thing; //another method of initializing tuples
+    thing = make_tuple(21,'c',43.23,false);
+
+    //swap values of two same typed tuples
+    tuple<int,char> t1(10,'a');
+    tuple<int,char> t2(20,'b');
+    t1.swap(t2);
+    cout << "t1: " <<get<0>(t1) << " " << get<1>(t1) << endl;
+    cout << "t2: " <<get<0>(t2) << " " << get<1>(t2) << endl;
+
+    //decompose tuple into variables
+    int x;
+    char y;
+    tie(x,y) = t2;
+    cout << x << " " << y << endl;
+
+    //concatonate tuples
+    tuple<int,string,int,char> t3 = tuple_cat(person,t1);
+    cout << "t3: " << get<0>(t3) << " " << get<1>(t3) << " " <<get<2>(t3) << " " << get<3>(t3) << endl;
 }
