f0aca4044590d2a71b4af51b75b5d564b7503145
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-14 20:54:42 +0530

    added classes.cpp

---
+++ classes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person {
+    public:
+        string name;
+        int age;
+        string nationality;
+
+
+};
+
+int main(){
+    Person p1;
+    p1.name = "Nikola Tesla";
+    p1.age =  166;
+    p1.nationality = "American";
+
+
+}
