0f9247d3e774b997b8df95b8f1891eaf743720ec
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-14 21:24:09 +0530

    modified classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -7,8 +7,27 @@ class Person {
         string name;
         int age;
         string nationality;
+};
 
+class Book {
+    //constructor
+    //basically an __init__ function
+    //one class can have multiple constructors, which will execute depending upon the arguments provided
+    public:    
+        string title;
+        string author;
+        int pages;
 
+        Book(){
+            cout << "No title provided" << '\n' << "No author provided" << '\n' << "No pages provided" << endl;
+        }
+
+        Book(string aTitle,string aAuthor,int aPages){
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+            cout << title << " " << author << " " << pages << endl;
+        }
 };
 
 int main(){
@@ -17,5 +36,7 @@ int main(){
     p1.age =  166;
     p1.nationality = "American";
 
+    Book b1;
+    Book b2("Wings of Fire","Kalam",300);
 
 }
